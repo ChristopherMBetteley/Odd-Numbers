03851966441e042611a60d5fb3a3026968df21bb
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-08 19:49:04 -0400

    first commit

---
+++ Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            
+            for(number = 1; number <= 99; number += 2)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
